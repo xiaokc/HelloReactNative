9f17f4638fccf59d6a5c6bdf6d9f838d807d4f9d
xiaokecong <xiaokecong@baidu.com>
2017-08-22 20:19:50 +0800

    原生模块暴露方法给js
    实现listFile等功能

--- android/app/src/main/java/com/helloreactnative/mymodule/RNFSManager.java
+++ android/app/src/main/java/com/helloreactnative/mymodule/RNFSManager.java
@@ -2,10 +2,12 @@ package com.helloreactnative.mymodule;
 
 import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Base64;
 import com.facebook.react.bridge.*;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.io.FileInputStream;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +16,14 @@ import java.util.Map;
  * Created by xiaokecong on 22/08/2017.
  */
 public class RNFSManager extends ReactContextBaseJavaModule {
-    private static final String E_LIST_FILES_EXCEPTION = "list_files_exception";
+    private static final String E_READ_DIR_EXCEPTION = "read_dir_exception";
     private static final String E_FILE_NOT_EXIST = "file_not_exist";
     private static final String E_FILE_PATH_IS_NULL = "file_path_is_null";
+    private static final String E_STAT_FILE_EXCEPTION = "stat_file_exception";
+    private static final String E_READ_FILE_EXCEPTION = "read_file_exceptiond";
 
     private static final String RNFSDCIMDirectoryPath = "RNFSDCIMDirectoryPath";
+    private static final String RNFSDownloadPath = "RNFSDownloadPath";
 
     public RNFSManager(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -30,11 +35,12 @@ public class RNFSManager extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void listFiles(String directoryPath, Promise promise) {
+    public void readDir(String directoryPath, Promise promise) {
         try {
             File dir = new File(directoryPath);
             if (!dir.exists()) {
-                promise.reject(E_FILE_NOT_EXIST, E_LIST_FILES_EXCEPTION);
+                promise.reject(E_FILE_NOT_EXIST, E_READ_DIR_EXCEPTION);
+                return;
             }
 
             File[] files = dir.listFiles();
@@ -54,7 +60,7 @@ public class RNFSManager extends ReactContextBaseJavaModule {
             promise.resolve(fileMaps);
         } catch (Exception e) {
             e.printStackTrace();
-            promise.reject(E_LIST_FILES_EXCEPTION, e.getMessage());
+            promise.reject(E_READ_DIR_EXCEPTION, e.getMessage());
         }
     }
 
@@ -76,9 +82,39 @@ public class RNFSManager extends ReactContextBaseJavaModule {
                 promise.resolve(sizeStr);
             } else {
                 promise.reject(E_FILE_NOT_EXIST, E_FILE_NOT_EXIST);
+                return;
             }
         } else {
             promise.reject(E_FILE_PATH_IS_NULL, E_FILE_PATH_IS_NULL);
+            return;
+        }
+    }
+
+    @ReactMethod
+    public void readFile(String filePath, Promise promise) {
+        try {
+            File file = new File(filePath);
+            if (file.isDirectory()) {
+                promise.reject(E_READ_FILE_EXCEPTION, "Path is a directory, not a file! ");
+                return;
+            }
+
+            if (!file.exists()) {
+                promise.reject(E_FILE_NOT_EXIST, E_FILE_NOT_EXIST);
+                return;
+            }
+
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] buffer = new byte[(int) file.length()];
+            inputStream.read(buffer);
+
+            String base64Content = Base64.encodeToString(buffer, Base64.NO_WRAP);
+            promise.resolve(base64Content);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            promise.reject(E_READ_FILE_EXCEPTION, e.getMessage());
         }
     }
 
@@ -88,6 +124,8 @@ public class RNFSManager extends ReactContextBaseJavaModule {
         final Map<String, Object> constants = new HashMap<>();
         constants.put(RNFSDCIMDirectoryPath,
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath());
+        constants.put(RNFSDownloadPath,
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath());
         return constants;
     }
 }
--- js/androidjs/RecogDetailPage.js
+++ js/androidjs/RecogDetailPage.js
@@ -9,39 +9,99 @@ import {
     StyleSheet,
     NativeModules,
     TouchableOpacity,
+    ListView,
 } from 'react-native';
 var RNFSManager = NativeModules.RNFSManager;
-
+var data = [];
 export default class RecogDetailPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             infoText: this.props.navigation.state.params.info,
+            isLoaded: false,
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 != row2,
+            }),
         }
     }
 
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        RNFSManager.readDir(RNFSManager.RNFSDownloadPath)
+            .then((resultArray) => {
+                for (var i = 0; i < resultArray.length; i++) {
+                    data.push(resultArray[i].path);
+                }
+                this.setState({
+                    isLoaded: true,
+                    dataSource: this.state.dataSource.cloneWithRows(data),
+                });
+            })
+            .catch((error) => {
+                alert("error:" + error.message);
+            });
+    }
+
+
     render() {
+
+        if (!this.state.isLoaded) {
+            return this.loadingView();
+        }
         return (
-            <View style={styles.container}>
-                <Text style={styles.infoTxt}>{this.state.infoText}</Text>
-                <TouchableOpacity onPress={() => {
-                    this.clickTry()
-                }}>
-                    <Text style={styles.infoText}>{'have a try'}</Text>
-                </TouchableOpacity>
-            </View>
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderFileListItem.bind(this)}/>
         );
     }
 
-    clickTry() {
-        RNFSManager.getFileSizeStr(RNFSManager.RNFSDCIMDirectoryPath + "/Camera/1501572112169.png")
+    loadingView() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.infoTxt}>{'加载中...'}</Text>
+            </View>
+        )
+    }
+
+    renderFileListItem = (path) => {
+        return (
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.listItem}
+                onPress={() => {
+                    this.onFileItemClick(path)
+                }}
+            >
+                <View style={styles.listItem}>
+                    <View style={styles.itemViewStyle}>
+                        <Text style={styles.itemTitle}>{path}</Text>
+
+                    </View>
+                </View>
+
+            </TouchableOpacity>
+        );
+    };
+
+    fetchItemData() {
+        var sizeStr = "";
+        RNFSManager.getFileSizeStr(path)
             .then((result) => {
-                alert(result);
+                sizeStr = result;
+                alert("getFileSize succ:" + result);
             })
             .catch((error) => {
-                alert("error:" + error);
+                alert("getFileSizeStr error:" + error);
             });
     }
+
+    onFileItemClick = (path) => {
+        alert(path);
+    }
+
 }
 
 const styles = StyleSheet.create({
@@ -53,5 +113,30 @@ const styles = StyleSheet.create({
         fontSize: 18,
         alignContent: 'center',
         margin: 20
+    },
+    listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
+        marginLeft: 3,
+        marginRight: 3,
+        borderBottomWidth: 1,
+        height:30,
+        borderBottomColor: '#cccccc',
+    },
+    itemViewStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginLeft: 10
+    },
+    itemTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'left'
+    },
+    itemTxt: {
+        fontSize: 15,
+        marginTop: 5
     }
 });
